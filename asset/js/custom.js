ff53618aea1bbca726a798332df24cd6f23fb7a5
Azmir Hossain <45963947+hasanazmir@users.noreply.github.com>
2021-09-16 12:41:40 +0600

    Add files via upload

---
+++ asset/js/custom.js
@@ -0,0 +1,15 @@
+
+
+
+$(".sponsor-customer-carousel").owlCarousel({
+            items: 4,
+            dots: true,
+            loop: true,
+            margin: 60,
+            nav: false,
+            animateOut: 'fadeOut',
+            animateIn: 'fadeIn',
+            responsiveClass: true,
+            navText: ["<i class='fas fa-chevron-left'></i>", "<i class='fas fa-chevron-right'></i>"],
+            
+        });
